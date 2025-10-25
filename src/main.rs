f6eb5d7a3d2d6b1d6e7dd2503bf8dc276a46cc2d
nonl4331 <nicholasl0003@gmail.com>
2025-10-25 13:27:52 +1100

    Added basic text rendering

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use winit::{
     window::WindowBuilder,
 };
 
+use ab_glyph::{Font, FontArc, FontRef, Glyph, PxScale, PxScaleFont, point};
+
 use pixels::{Pixels, SurfaceTexture};
 
 const WIDTH: u32 = 324;
@@ -67,16 +69,24 @@ struct World {
     velocity_x: i16,
     velocity_y: i16,
     frog: Bitmap,
+    font: PxScaleFont<FontArc>,
 }
 
 impl World {
     fn new() -> Self {
+        let font = ab_glyph::FontArc::try_from_slice(include_bytes!("../res/font1.otf")).unwrap();
+        let font = PxScaleFont {
+            font,
+            scale: PxScale { x: 16.0, y: 16.0 },
+        };
+
         Self {
             box_x: 24,
             box_y: 16,
             velocity_x: 1,
             velocity_y: 1,
-            frog:  Bitmap::new("res/frog1.png"),
+            frog: Bitmap::new("res/frog1.png"),
+            font,
         }
     }
 
@@ -110,6 +120,14 @@ impl World {
 
             pixel.copy_from_slice(&rgba);
         }
+        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+            let x = (i % WIDTH as usize) as i16 / FROG_PIXEL_WIDTH;
+            let y = (i / WIDTH as usize) as i16 / FROG_PIXEL_WIDTH;
+
+            if let Some(col) = self.frog.query_pixel(x as u32, y as u32) {
+                pixel.copy_from_slice(&col);
+            }
+        }
         // frog layer
         for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
             let x = (i % WIDTH as usize) as i16 / FROG_PIXEL_WIDTH;
@@ -118,7 +136,59 @@ impl World {
             if let Some(col) = self.frog.query_pixel(x as u32, y as u32) {
                 pixel.copy_from_slice(&col);
             }
+        }
+        const TEXT_PIXELS_WIDE: usize = 100;
+        const TEXT_PIXELS_HIGH: usize = 100;
+        let mut font_buffer = vec![[0u8; 4]; TEXT_PIXELS_HIGH * TEXT_PIXELS_WIDE];
 
+        use ab_glyph::ScaleFont;
+        // text layer
+        let test_text = "H";
+        let mut caret = point(0.0, 16.0);
+        let mut prev = None;
+        for c in test_text.chars() {
+            let id = self.font.glyph_id(c);
+            if let Some(prev_id) = prev {
+                caret.x += self.font.kern(prev_id, id);
+            }
+            let mut glyph = id.with_scale_and_position(16.0, caret);
+            glyph.position.x = glyph.position.x.round();
+            glyph.position.y = glyph.position.y.round();
+            if let Some(bm) = self.font.outline_glyph(glyph) {
+                bm.draw(|x, y, v| {
+                    if v > 0.5 {
+                        font_buffer[x as usize + y as usize * TEXT_PIXELS_WIDE] = [255, 255, 255, 255];
+                    }
+                })
+            }
+            prev = Some(id);
+            caret.x += self.font.h_advance(id);
+        }
+
+        const TEXT_PIXEL_SIZE: usize = 16;
+        const TEXT_PIXEL_OFFSET_X: usize = 40;
+        const TEXT_PIXEL_OFFSET_Y: usize = 40;
+        let xy_to_tx_pixel = |x: usize, y: usize| {
+            if x < TEXT_PIXEL_OFFSET_X || y < TEXT_PIXEL_OFFSET_Y {
+                return None;
+            }
+            let x = (x - TEXT_PIXEL_OFFSET_X) / TEXT_PIXEL_SIZE;
+            let y = (y - TEXT_PIXEL_OFFSET_Y) / TEXT_PIXEL_SIZE;
+            if x < TEXT_PIXELS_WIDE && y < TEXT_PIXELS_HIGH {
+                return Some(font_buffer[x as usize + y as usize * TEXT_PIXELS_WIDE]);
+            }
+            None
+        };
+        // draw text layer
+        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+            let x = (i % WIDTH as usize) as i16;
+            let y = (i / WIDTH as usize) as i16;
+
+            if let Some(col) = xy_to_tx_pixel(x as usize, y as usize) {
+                if col[3] != 0 {
+                    pixel.copy_from_slice(&col);
+                }
+            }
         }
     }
 }
